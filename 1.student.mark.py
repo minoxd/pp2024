dd70f6a08833998f77b4ac754bdf8782916c443f
unknown <minophen.aceta114@gmail.com>
2024-01-04 00:59:12 +0700

    first func of pw1

---
+++ 1.student.mark.py
@@ -0,0 +1,9 @@
+def makeClassOf(numOfStudents):
+    for i in range(numOfStudents):
+        studentID = input("Enter Student ID: ")
+        studentName = input("Enter Student Name: ")
+        studentDOB = input("Enter Student DOB: ")
+
+
+numberOfStudents = int(input("Enter number of students: "))
+makeClassOf(numberOfStudents)
