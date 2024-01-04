986ba38f8ab7a8e3027476fc2a90654a141d0484
minoxd <minophen.aceta114@gmail.com>
2024-01-04 14:53:47 +0700

    update pw1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,8 +1,23 @@
+def newClass(listCLASS, currentNumClass):
+    currentNumClass += 1
+    s = "CLASS" + str(currentNumClass)
+    listCLASS += [s]
+    return currentNumClass
 def makeClassOf(numOfStudents):
     for i in range(numOfStudents):
         studentID = input("Enter Student ID: ")
         studentName = input("Enter Student Name: ")
         studentDOB = input("Enter Student DOB: ")
 
+def main():
+    #init
+    CLASS = []
+    numOfClass = 0
 
-CLASS = ()
+    #test
+    numOfClass = newClass(CLASS, numOfClass)
+    numOfClass = newClass(CLASS, numOfClass)
+    print(*CLASS)
+
+if __name__ == "__main__":
+    main()
