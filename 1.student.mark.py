9098c1e6802bbb02aa4038b3cb08b949e55f5234
minoxd <minophen.aceta114@gmail.com>
2024-01-07 21:38:25 +0700

    update

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -30,7 +30,7 @@ def home_option(list_class):
             case 4:
                 return
             case _:
-                print("Invalid option!")
+                print("\tInvalid option!")
 
 
 def class_student(list_class):
@@ -54,13 +54,13 @@ def class_student(list_class):
             case 1:
                 list_class = add_class(list_class)
             case 2:
-                list_class = delete_class(list_class)
+                list_class = del_class(list_class)
             case 3:
                 list_class = update_student(list_class)
             case 4:
                 display(list_class)
             case _:
-                print("Invalid option!")
+                print("\tInvalid option!")
 
 
 def check_exist(list_class, name):
@@ -70,13 +70,12 @@ def check_exist(list_class, name):
     return False
 
 
-# combine with get student
 def get_name(the_list, index):
     return the_list[index]["name"]
 
 
 def add_class(list_class):
-    new_name = str(input("Enter class name (or leave a blank for default): "))
+    new_name = str(input("Enter class name (or leave a blank): "))
     if new_name == "":
         new_name = "unnamed"
         while check_exist(list_class, new_name):
@@ -90,15 +89,15 @@ def add_class(list_class):
     return list_class
 
 
-def class_not_exist(list_class):
-    num_class = len(list_class)
-    if num_class < 1:
-        print("There is no class.")
+def list_no_element(the_list, class_or_student: int):
+    num_element = len(the_list)
+    if num_element < 1:
+        print(f"There is no {["class", "student"][class_or_student-1]}.")
         return True
     return False
 
 
-def print_get_class_choice(list_class):
+def print_get_class_choice(list_class):  # todo: combine with get student
     num_class = len(list_class)
     while True:
         print("""
@@ -115,11 +114,11 @@ def print_get_class_choice(list_class):
         if 0 <= option <= num_class:
             return option
         else:
-            print("Invalid option!")
+            print("\tInvalid option!")
 
 
-def delete_class(list_class):
-    if class_not_exist(list_class):
+def del_class(list_class):
+    if list_no_element(list_class, 1):
         return list_class
     class_selection = print_get_class_choice(list_class)
     if class_selection == 0:
@@ -130,7 +129,7 @@ def delete_class(list_class):
 
 
 def update_student(list_class):
-    if class_not_exist(list_class):
+    if list_no_element(list_class, 1):
         return list_class
     class_selection = print_get_class_choice(list_class)
     if class_selection == 0:
@@ -144,12 +143,12 @@ def update_student_option(list_class, class_selection):
 
     while True:
         print(f"""
-        {get_name(list_class, class_selection - 1)}
+        Class: {get_name(list_class, class_selection - 1)}
 
 [0] Exit
 [1] Add a new student
 [2] Add multiple students
-[3] Delete the newest student
+[3] Delete a student
 [4] Delete multiple students
 [5] Delete all
 [6] Update an existing student
@@ -165,13 +164,13 @@ def update_student_option(list_class, class_selection):
             case 1:
                 list_class[class_selection - 1]["student"] = add_student(list_class[class_selection - 1]["student"])
             case 2:
-                return
-            case 3: # do shift id when delete
-                return
+                list_class[class_selection - 1]["student"] = add_n_student(list_class[class_selection - 1]["student"])
+            case 3:
+                list_class[class_selection - 1]["student"] = del_student(list_class[class_selection - 1]["student"])
             case 4:
                 return
             case _:
-                print("Invalid option!")
+                print("\tInvalid option!")
 
 
 def add_student(list_student):
@@ -195,12 +194,49 @@ def get_dob():
             )
             break
         except ValueError as ve:
-            print(f"\nInvalid input: {ve}!")
+            print(f"\n\tInvalid input: {ve}!")
     return dob
 
 
+def ordinal(n: int):
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    else:
+        suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+    return str(n) + suffix
+
+
+def add_n_student(list_student):
+    while True:
+        try:
+            times = int(input("Enter number of new students ([0] to exit): "))
+        except ValueError:
+            times = -1
+        if times == 0:
+            return list_student
+        if times < 0:
+            print("\tInvalid input!")
+        else:
+            for i in range(times):
+                print(f"\n\tEnter information for the {ordinal(i+1)} student: ")
+                list_student = add_student(list_student)
+            break
+    return list_student
+
+
+def del_student(list_student):  # todo: need change
+    if list_no_element(list_student, 2):
+        return list_student
+    class_selection = print_get_class_choice(list_student)
+    if class_selection == 0:
+        return list_student
+    print("Deleted", get_name(list_student, class_selection - 1))
+    del list_student[class_selection - 1]
+    return list_student
+
+
 def display(list_class):
-    if class_not_exist(list_class):
+    if list_no_element(list_class, 1):
         return
     num_class = len(list_class)
     if num_class == 1:
