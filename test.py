eb3f7c9d7cd70efdbb7be4b694d2d8d5890edb9d
minoxd <minophen.aceta114@gmail.com>
2024-03-13 06:06:06 +0700

    m

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import math
+import numpy as np
 
-# Get student score as input
-raw_score = float(input("Enter student score: "))
-
-# Round down to 1-digit decimal using math.floor()
-rounded_score = math.floor(raw_score * 10) / 10
-
-# Print the rounded score
-print("Rounded down score:", rounded_score)
-
-print(math.floor(0 * 10) / 10)
+np.array()
