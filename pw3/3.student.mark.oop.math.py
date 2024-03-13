b634bd688003b5334b4663c5ed5d434d6841d68d
minoxd <minophen.aceta114@gmail.com>
2024-03-13 10:27:52 +0700

    update

--- pw3/3.student.mark.oop.math.py
+++ pw3/3.student.mark.oop.math.py
@@ -2,14 +2,52 @@ import math
 from datetime import date
 
 
+# todo private method
+# todo only change, no add mark
+# todo a mark for course not found, use default
+def home(s_list, c_list):
+    while True:
+        print("""
+        STUDENT MARK PROGRAM
+
+[0] Exit
+[1] Students
+[2] Courses
+[3] Quick options
+""")
+        try:
+            select = int(input("Enter your choice: "))
+        except ValueError:
+            select = -1
+        match select:
+            case 0:
+                print("""
+        THANK YOU FOR USING MY PROGRAMME!!""")
+                return s_list, c_list, m_list
+            case 1:
+                s_list, m_list = student(s_list, m_list)
+            case 2:
+                s_list, c_list, m_list = course(s_list, c_list, m_list)
+            case 3:
+                s_list, c_list, m_list = quick(s_list, c_list, m_list)
+            case _:
+                print("Invalid option!")
+
+
 class Student:
-    __list_sid = []
+    __list_sid: list = []
+    __default_sid: str = ""
+    __default_sname: str = "student_name"
+    __default_sdob: date = date(1, 1, 1)
+    __default_smark: list = []
+    __default_gpa: float = math.floor(0 * 10) / 10
 
     def __init__(self):
-        self.__sid = str
-        self.__sname = str
-        self.__sdob = date
-        self.__smark = list
+        self.__sid: str = self.__default_sid
+        self.__sname: str = self.__default_sname
+        self.__sdob: date = self.__default_sdob
+        self.__smark: list = self.__default_smark
+        self.__gpa: float = self.__default_gpa
 
     # getters
     def get_list_sid(self):
@@ -29,6 +67,16 @@ class Student:
 
     def get_smark(self):
         return self.__smark
+
+    def get_gpa(self):
+        if len(self.__smark) == 0:
+            return self.__default_gpa
+        c = Course()
+        # todo
+        for m in self.__smark:
+            pass
+        self.__gpa = 0
+        return self.__gpa
 
     # setters
     def append_list_sid(self, sid):
@@ -60,7 +108,7 @@ class Student:
     def set_sname(self):
         sname = input("Enter student name: ")
         if not sname:
-            sname = "student_name"
+            sname = self.__default_sname
         self.__sname = sname
 
     def set_sdob(self):
@@ -71,24 +119,35 @@ class Student:
                 m = input("\tMonth: ")
                 d = input("\tDay: ")
                 if not y and not m and not d:
-                    y = 1
-                    m = 1
-                    d = 1
+                    sdob = self.__default_sdob
+                    break
                 sdob = date(int(y), int(m), int(d))
                 break
             except ValueError as ve:
                 print(f"\nInvalid input: {ve}!")
         self.__sdob = sdob
 
+    def append_smark(self, smark):
+        self.__smark.append(smark)
+
+    def remove_smark(self, smark):
+        self.__smark.remove(smark)
+
+    def delete_smark(self):
+        self.__smark.clear()
+
 
 class Course:
     __list_cid: list = []
     __sum_cre: int = 0
+    __default_cid: str = ""
+    __default_cname: str = "course_name"
+    __default_cre: int = 0
 
     def __init__(self):
-        self.__cid = str
-        self.__cname = str
-        self.__cre = int
+        self.__cid = self.__default_cid
+        self.__cname = self.__default_cname
+        self.__cre = self.__default_cre
 
     # getters
     def get_list_cid(self):
@@ -96,6 +155,9 @@ class Course:
             print("No course id used yet!")
             return
         print("Used course ids: " + str(self.__list_cid))
+
+    def get_sum_cre(self):
+        return self.__sum_cre
 
     def get_cid(self):
         return self.__cid
@@ -112,6 +174,12 @@ class Course:
 
     def remove_list_cid(self, cid):
         self.__list_cid.remove(cid)
+
+    def add_sum_cre(self, cre):
+        self.__sum_cre += cre
+
+    def minus_sum_cre(self, cre):
+        self.__sum_cre -= cre
 
     def set_cid(self):
         while True:
@@ -133,7 +201,7 @@ class Course:
     def set_cname(self):
         cname = input("Enter course name: ")
         if not cname:
-            cname = "course_name"
+            cname = self.__default_cname
         self.__cname = cname
 
     def set_cre(self):
@@ -141,7 +209,7 @@ class Course:
             try:
                 cre = input("Enter credit value: ")
                 if not cre:
-                    cre = 0
+                    cre = self.__default_cre
                 cre = int(cre)
                 break
             except ValueError:
@@ -150,9 +218,12 @@ class Course:
 
 
 class Mark:
-    def __init__(self, mcid, mval=math.floor(0 * 10) / 10):
+    """Store locally in each student"""
+    __default_mval: float = math.floor(0 * 10) / 10
+
+    def __init__(self, mcid):
         self.__mcid = mcid
-        self.__mval = mval
+        self.__mval = self.__default_mval
 
     # getters
     def get_mcid(self):
@@ -167,7 +238,8 @@ class Mark:
             try:
                 mval = input("Enter new mark (scale 20): ")
                 if not mval:
-                    mval = float(0)
+                    self.__mval = self.__default_mval
+                    break
                 mval = float(mval)
             except ValueError:
                 mval = -1
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+import math
+
 import numpy as np
 
-np.array()
+numpy_score = np.array([20, 18, 16])
+numpy_score_weight = np.array([.1, .3, .6])
+
+print(numpy_score * numpy_score_weight)
+print(math.floor(np.sum(numpy_score * numpy_score_weight) * 10) / 10)
+print(20 * .1 + 18 * .3 + 16 * .6)
