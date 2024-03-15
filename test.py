c1371394336e784e1132f3a46619cf4b66793b94
minoxd <minophen.aceta114@gmail.com>
2024-03-15 08:28:55 +0700

    update

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import curses
 from curses import wrapper
 from curses.textpad import Textbox
-error = []
+error = []  # todo
 warn = []
+
 
 def menu_home(stdscr):
     option = [
@@ -15,15 +16,17 @@ def menu_home(stdscr):
     stdscr.addstr(1, 8, "STUDENT MARK PROGRAM")
     for i in range(4):
         stdscr.addstr(3 + i, 0, f"[{i}] {option[i]}")
-    stdscr.addstr(8, 0, "Enter your choice: ")
-    win = curses.newwin(1, 20, 8, 19)
-    box = Textbox(win)
+
+    stdscr.addstr(15, 0, "Enter your choice: ")
     stdscr.refresh()
+    win = curses.newwin(1, 30, 15, 19)
+    box = Textbox(win)
+
     curses.curs_set(1)
     box.edit()
     curses.curs_set(0)
     text = box.gather()
-    stdscr.addstr(10, 0, f"{str(type(int(text)))}: {text}")
+    stdscr.addstr(16, 0, f"{str(type(text))}: {text}")
     stdscr.getch()
 
 
--- test2.py
+++ test2.py
@@ -1,14 +1,44 @@
+import curses
+import sys
+import time
 from curses import wrapper
+from curses.textpad import Textbox
+error = []  # todo
+warn = []
+
+
+def test_screen(stdscr):
+
+    while True:
+        try:
+            pad = curses.newpad(20, 50)
+            stdscr.refresh()
+
+            for i in range(999):
+                pad.addstr(" ")
+
+            for i in range(50):
+                stdscr.clear()
+                stdscr.refresh()
+                pad.refresh(0, 0, 0, 0, 0 + i, 0 + i)
+                time.sleep(.0001)
+            stdscr.getch()
+            break
+        except curses.error:
+            stdscr.clear()
+            stdscr.refresh()
+            curses.init_pair(1, curses.COLOR_RED, -1)
+            stdscr.addstr("Please make your window larger!", curses.color_pair(1))
+            stdscr.getch()
 
 
 def main(stdscr):
-    stdscr.clear()
-
-    for i in range(0, 11):
-        v = i-10
-        stdscr.addstr(i, 0, "10 divided by {} is {}".format(v, 10/v))
-        stdscr.refresh()
-        stdscr.getkey()
+    curses.use_default_colors()
+    curses.curs_set(0)
+    test_screen(stdscr)
 
 
-wrapper(main)
+try:
+    wrapper(main)
+except KeyboardInterrupt:
+    print("\033[91m" + "\n\nForced to exit using keyboard!" + "\033[0m")
